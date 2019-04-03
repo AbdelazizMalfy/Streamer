a7841b23c91fd2c4b9db70ce70d049d63073d737
Abdelaziz Mohamed <Abdelazizmalfy@gmail.com>
2019-04-03 22:53:30 +0200

    form added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import {  combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import AuthReducers from './AuthReducer'
 
 
 export default combineReducers({
-   auth : AuthReducers 
+   auth: AuthReducers ,
+   form: formReducer
 });
